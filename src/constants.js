ddea03ae9d7a8591ba995ca074a6750ed3d65370
Leonel Aguirre <leonel.aguirre77@gmail.com>
2022-08-29 07:25:03 -0500

    :sparkles: L-System being correctly drew in the P5 canvas.

---
+++ src/constants.js
@@ -0,0 +1,3 @@
+export const LINE_LENGTH = 20
+export const WIDTH = 600
+export const HEIGHT = 600
--- src/index.js
+++ src/index.js
@@ -2,6 +2,15 @@ import "./index.css"
 
 import p5 from "p5"
 
+import { WIDTH, HEIGHT } from "./constants"
+import LSystem from "./LSystem/LSystem"
+import FractalPlant1 from "./data/FractalPlant1"
+import FractalPlant2 from "./data/FractalPlant2"
+import DragonCurve from "./data/DragonCurve"
+import SierpinskiTriangle from "./data/SierpinskiTriangle"
+import SierpinskiTriangle2 from "./data/SierpinskiTriangle2"
+import Tiles from "./data/Tiles"
+
 /**
  * Displays a p5 sketch.
  *
@@ -9,19 +18,28 @@ import p5 from "p5"
  */
 const sketch = (p) => {
   // Editable values.
-  const WIDTH = 600
-  const HEIGHT = 600
+
+  let lSystem
 
   // Sketch setup.
   p.setup = () => {
     let canvas = p.createCanvas(WIDTH, HEIGHT)
     canvas.parent("Canvas")
+    p.translate(WIDTH / 2, HEIGHT / 2)
+
+    const { alphabet, initiator, rules, instructions } = Tiles
+
+    lSystem = new LSystem(alphabet, initiator, rules, instructions)
+
+    const lSystemIteration = 3
+
+    console.log(lSystem.getIterationValue(lSystemIteration))
+
+    LSystem.drawLSystemPattern(p, lSystem, lSystemIteration)
   }
 
   // Draw loop.
-  p.draw = () => {
-    p.background("white")
-  }
+  p.draw = () => {}
 }
 
 new p5(sketch)
